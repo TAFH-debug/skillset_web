3b0e037330bc1824933001784e372c436630081c
Xelzoid <zanggar.manarbek@icloud.com>
2025-05-17 15:33:04 +0500

    pushing to prod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,47 @@
 "use client";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
-
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
+import { Accordion, AccordionItem } from "@heroui/accordion";
+import Block from "@/components/block";
 import { title } from "@/components/primitives";
-
+import { Upload } from 'lucide-react';
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>
-          Next.JS + HeroUI + DrizzleORM + Neon template
-        </span>
-      </div>
+    <div className="relative flex flex-col items-center overflow-x-hidden ">
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 w-screen">
+        <div className="inline-block max-w-xl text-center justify-center mb-8 ">
+            <span className={title()}>
+            Fast AI Video Editing
+            </span>
+        </div>
+        <div className="cursor-pointer">
+          <Button size="lg" color="primary" className="" endContent={<Upload size={16}/>}>Get started</Button>
+        </div>
+      </section>
+      <section className="mt-20">
+        <div id="scroll" className="flex flex-col md:flex-row gap-20 justify-center items-stretch max-w-screen mb-10">
+        <div  >
+          <Block BlockTitle="Что делает нашу платформу уникальной?"/>
+          <Accordion variant="shadow">
+            <AccordionItem key="1" title="Сценарные симуляции">Моделирование реальных ситуаций (захват заложников, киберпреступления, патрулирование).</AccordionItem>
+            <AccordionItem key="2" title="Анализ действий">Подробный разбор результатов и персонализованные рекомендации по улучшению.</AccordionItem>
+            <AccordionItem key="3" title="Многопользовательский режим">Совместные тренировки и отработка взаимодействия в команде.</AccordionItem>
+            <AccordionItem key="4" title="Виртуальная реальность">Глубокое погружение для максимальной реалистичности.</AccordionItem>
+            <AccordionItem key="5" title="Теоретический блок">Доступ к справочникам, нормативным актам и учебным материалам.</AccordionItem>
+          </Accordion>
+        </div>
+        <div>
+          <Block BlockTitle="Как наша платформа улучшает навыки?"/>
+          <Accordion variant="shadow" >
+            <AccordionItem key="1" title="Регистрация">Создайте учетную запись и получите доступ к платформе.</AccordionItem>
+            <AccordionItem key="2" title="Выбор сценария">Выберите интересующую ситуацию для тренировки (например, патруль, переговоры с преступником и т. д.).</AccordionItem>
+            <AccordionItem key="3" title="Выполнение заданий">Выполняйте задания в режиме VR или на обычном компьютере, анализируйте результаты.</AccordionItem>
+            <AccordionItem key="4" title="Получение обратной связи">Изучайте отчеты по эффективности, улучшайте навыки.</AccordionItem>
+          </Accordion>
 
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
+        </div>
       </div>
-    </section>
+      </section>
+    </div>
   );
 }
